02f7f1520c3413befc85d9e486e840ef5ff6dbc6
lomeat <lom3at@gmail.com>
2020-08-29 17:04:37 +0300

    add: todolist with todo complete toggle state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-export const App = () => (
-  <>
-    <H1>Hello there</H1>
-    <H1 red>This text is red</H1>
-  </>
-);
+export const App = () => {
+  const [todos, setTodos] = useState({
+    list: [
+      {
+        id: 1,
+        title: "Do homework",
+        isComplete: false,
+        children: [],
+      },
+      {
+        id: 2,
+        title: "Buy milk",
+        isComplete: false,
+        children: [],
+      },
+      {
+        id: 3,
+        title: "Get an offer",
+        isComplete: false,
+        children: [],
+      },
+    ],
+    completed: [],
+  });
 
-const H1 = styled.h1`
-  color: ${(props) => (props.red ? "red" : "black")};
+  // toggle complete state of one todo
+  // and move to down of list if completed
+  // or move to up of list if uncompleted
+  const handleClickTodoTitleButton = (todo) => {
+    if (todo.isComplete) {
+      setTodos((state) => ({
+        list: [...state.list, { ...todo, isComplete: false }],
+        completed: state.completed.filter((item) => item.id !== todo.id),
+      }));
+    } else {
+      setTodos((state) => ({
+        list: state.list.filter((item) => item.id !== todo.id),
+        completed: [...state.completed, { ...todo, isComplete: true }],
+      }));
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Container>
+        {todos.list.map((todo) => (
+          <div key={todo.id}>
+            <Title onClick={() => handleClickTodoTitleButton(todo)}>
+              {todo.title}
+            </Title>
+          </div>
+        ))}
+        {todos.completed.map((todo) => (
+          <div key={todo.id}>
+            <Title onClick={() => handleClickTodoTitleButton(todo)} isComplete>
+              {todo.title}
+            </Title>
+          </div>
+        ))}
+      </Container>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  width: 700px;
+  padding: 10px;
+`;
+
+const Title = styled.h2`
+  text-decoration: ${(props) => (props.isComplete ? "underline" : "none")};
+  cursor: pointer;
+  transition: 0.1s ease;
+  padding: 4px;
+  border-radius: 4px;
+  :hover {
+    background: #eee;
+  }
 `;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
 import "normalize.css";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    user-select: none;
+    outline: none;
+    box-sizing: border-box;
+  }
+`;
 
 import { App } from "./App";
 
-render(<App />, document.getElementById("root"));
+render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById("root")
+);
