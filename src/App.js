5c27af429b8596c5a62e8d2d5b44f2a60e50264e
lomeat <lom3at@gmail.com>
2020-09-03 05:43:11 +0300

    ref: move all logic to App and update global one state with bruteforce

--- src/App.js
+++ src/App.js
@@ -2,13 +2,64 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 import { TodoTree } from "./TodoTree";
-import { todosMock } from "./mock";
+import todosMock from "./mock";
 
 export const App = () => {
+  const [todos, setTodos] = useState(todosMock);
+
+  const updateTodos = (todos, id, action) => {
+    let newTodos = {};
+    const bruteforce = (todos, temp = {}, action) => {
+      for (let key in todos) {
+        if (key === "children") {
+          temp[key] = todos[key];
+          for (let anotherKey in todos[key]) {
+            switch (action) {
+              case "toggle": {
+                const oldElement = todos[key][anotherKey];
+                const newElement = {
+                  ...oldElement,
+                  isComplete: !oldElement.isComplete,
+                };
+                if (oldElement.id === id) {
+                  temp[key][anotherKey] = newElement;
+                } else {
+                  temp[key][anotherKey] = oldElement;
+                }
+              }
+            }
+
+            if (!temp[key][anotherKey].children.length) {
+              newTodos = { ...temp };
+            }
+
+            bruteforce(todos[key][anotherKey], temp[key][anotherKey], action);
+          }
+        }
+      }
+    };
+
+    bruteforce(todos, newTodos, action);
+    setTodos(newTodos);
+  };
+
+  const toggleIsTodoComplete = (todo) => {
+    updateTodos(todos, todo.id, "toggle");
+  };
+
+  const addNewTodo = (todo) => {};
+
+  const removeTodo = (id) => {};
+
   return (
     <Wrapper>
       <Container>
-        <TodoTree children={todosMock} />
+        <TodoTree
+          children={todos.children}
+          addNewTodo={addNewTodo}
+          toggleIsTodoComplete={toggleIsTodoComplete}
+          removeTodo={removeTodo}
+        />
       </Container>
     </Wrapper>
   );
