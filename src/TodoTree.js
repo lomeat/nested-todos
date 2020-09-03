c56c7b34b2640e61027c8d1beeff61a442a7a5db
lomeat <lom3at@gmail.com>
2020-09-03 05:40:16 +0300

    ref: remove all logic with minor states from todo tree (children)

--- src/TodoTree.js
+++ src/TodoTree.js
@@ -1,93 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { Todo } from "./Todo";
 
-export const TodoTree = ({ nestedLevel = 0, children }) => {
-  const [todos, setTodos] = useState(children);
-
-  // WD: Toggle complete state of one todo
-  //     if click when completed: move todo to up of line-throughed completed list
-  //     else: move todo to down of usual idle todo list
-  // HW: When click on completed todo return new state with adding element to list[]
-  //     and filtered completed[] without clicked todo
-  //     When click on uncompleted todo return new state with filtered list[] without clicked todo
-  //     and adding element to completed[]
-  const toggleIsTodoComplete = (todo) => {
-    if (todo.isComplete) {
-      setTodos((state) => ({
-        list: [...state.list, { ...todo, isComplete: false }],
-        completed: state.completed.filter((item) => item.id !== todo.id),
-      }));
-    } else {
-      setTodos((state) => ({
-        list: state.list.filter((item) => item.id !== todo.id),
-        completed: [{ ...todo, isComplete: true }, ...state.completed],
-      }));
-    }
-  };
-
-  // WD: Remove exist todo from list
-  // HW: If todo is complete return state with filtered completed[] without clicked todo
-  const removeTodo = (todo) => {
-    if (todo.isComplete) {
-      setTodos((state) => ({
-        ...state,
-        completed: state.completed.filter((item) => item.id !== todo.id),
-      }));
-    }
-  };
-
-  // WD: Add new todo to exist todo
-  // HW: If todo is uncompleted return previous state with
-  //     and adding new template todo to inner tree of clicked todo
-  const addNewTodo = (todo) => {
-    const newTodo = {
-      id: Math.floor(Math.random() * Date.now()),
-      title: "New Todo",
-      isComplete: false,
-      children: {},
-    };
-
-    if (!todo.isComplete) {
-      setTodos((state) => ({
-        ...state,
-        list: [
-          ...state.list,
-          {
-            ...todo,
-            children: {
-              // If todo.children object is empty, create new arrays to "list" and "completed" keys
-              list: todo.children.list
-                ? [...todo.children.list, newTodo]
-                : [newTodo],
-              completed: [],
-            },
-          },
-        ],
-      }));
-    }
-  };
-
-  return (
-    <>
-      {todos.list.map((todo) => (
+export const TodoTree = ({
+  nestedLevel = 0,
+  children,
+  removeTodo,
+  toggleIsTodoComplete,
+  addNewTodo,
+}) => (
+  <>
+    {children
+      .filter((todo) => todo.isComplete === false)
+      .map((todo) => (
         <Todo
           key={todo.id}
           todo={todo}
           nestedLevel={nestedLevel}
-          toggleIsComplete={toggleIsTodoComplete}
+          toggleIsTodoComplete={toggleIsTodoComplete}
           addNewTodo={addNewTodo}
-        />
-      ))}
-      {todos.completed.map((todo) => (
-        <Todo
-          key={todo.id}
-          todo={todo}
-          nestedLevel={nestedLevel}
-          toggleIsComplete={toggleIsTodoComplete}
           removeTodo={removeTodo}
         />
       ))}
-    </>
-  );
-};
+    {children
+      .filter((todo) => todo.isComplete === true)
+      .map((todo) => (
+        <Todo
+          key={todo.id}
+          todo={todo}
+          nestedLevel={nestedLevel}
+          toggleIsTodoComplete={toggleIsTodoComplete}
+          addNewTodo={addNewTodo}
+          removeTodo={removeTodo}
+        />
+      ))}
+  </>
+);
