262e00d20c3abf28c9ab1e3a842773db6e92f30a
lomeat <lom3at@gmail.com>
2020-09-10 08:58:47 +0300

    docs: update mock

--- src/mock.ts
+++ src/mock.ts
@@ -9,21 +9,29 @@ export const newTodoMock = {
 export const todosMock = {
   children: [
     {
-      id: 123,
-      title: "Hello! Some examples of using app",
-      isComplete: false,
-      isShowChildren: true,
-      children: [],
-    },
-    {
-      id: 121233,
-      title: "You can nest todo each in other",
+      id: 29879,
+      title: "Hello, its usual minimal todo list",
       isComplete: false,
       isShowChildren: true,
       children: [
         {
-          id: 234,
-          title: "And it is the first nested level",
+          id: 123,
+          title: "On React/Redux + TypeScript",
+          isComplete: false,
+          isShowChildren: true,
+          children: [
+            {
+              id: 456,
+              title: "You can nest each in other to 3 levels",
+              isComplete: false,
+              isShowChildren: true,
+              children: [],
+            },
+          ],
+        },
+        {
+          id: 345,
+          title: "Or create another tree",
           isComplete: false,
           isShowChildren: true,
           children: [],
@@ -31,69 +39,9 @@ export const todosMock = {
       ],
     },
     {
-      id: 432,
-      title: "Or make 3-level nested tree!",
-      isComplete: false,
-      isShowChildren: true,
-      children: [
-        {
-          id: 3452,
-          title: "At textfield above you add todos to main list",
-          isComplete: false,
-          isShowChildren: true,
-          children: [
-            {
-              id: 25234,
-              title: "But try to click '+' button there ->",
-              isComplete: false,
-              isShowChildren: true,
-              children: [
-                {
-                  id: 6154,
-                  title: "At last nested level you cannot add anything",
-                  isComplete: false,
-                  isShowChildren: true,
-                  children: [],
-                },
-                {
-                  id: 56745,
-                  title: "Oh, it's already completed!",
-                  isComplete: true,
-                  isShowChildren: true,
-                  children: [],
-                },
-              ],
-            },
-          ],
-        },
-        {
-          id: 3412352,
-          title: "Chevron helps to show/hide unnecessary deals",
-          isComplete: false,
-          isShowChildren: false,
-          children: [
-            {
-              id: 45234,
-              title: "All data save to local cache",
-              isComplete: false,
-              isShowChildren: false,
-              children: [],
-            },
-            {
-              id: 345345,
-              title: "So try to edit something and reload page!",
-              isComplete: false,
-              isShowChildren: false,
-              children: [],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 29879,
-      title: "WARNING! Trash icon - DELETE everything!",
-      isComplete: false,
+      id: 2431,
+      title: "Other checked category",
+      isComplete: true,
       isShowChildren: true,
       children: [],
     },
